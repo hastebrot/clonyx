3228c144cd1501e9eba493917d5e6c60160c19ee
Benjamin Gudehus <hastebrot@gmail.com>
2024-02-20 02:08:47 +0100

    feat: onyx-button implemented. (#2)

--- onyx-react/src/helper/classes.ts
+++ onyx-react/src/helper/classes.ts
@@ -1,7 +1,27 @@
 import clsx, { type ClassValue } from "clsx";
 
-export const className = (...inputs: ClassValue[]): { className: string } => {
-  return { className: clsx(inputs) };
-};
-
 export { clsx as classNames };
+
+export const classNamesUniq = (...inputs: ClassValue[]): string => {
+  function parseCssVariableOrNull(input: string) {
+    if (input.startsWith("[--") && input.includes(":")) {
+      return input.slice(1, input.indexOf(":"));
+    }
+    return null;
+  }
+
+  const items = clsx(...inputs).split(" ");
+  const resultItems: string[] = [];
+  const cssVariablesSet = new Set<string>();
+  for (const item of items.reverse()) {
+    const cssVariable = parseCssVariableOrNull(item);
+    if (cssVariable !== null) {
+      if (cssVariablesSet.has(cssVariable)) {
+        continue;
+      }
+      cssVariablesSet.add(cssVariable);
+    }
+    resultItems.push(item);
+  }
+  return resultItems.reverse().join(" ");
+};
--- onyx-react/src/onyx-theme/theme.tsx
+++ onyx-react/src/onyx-theme/theme.tsx
@@ -1,4 +1,7 @@
 import { classNames } from "../helper/classes";
+import "./variables-onyx.css";
+import "./variables-light.css";
+import "./variables-dark.css";
 
 export type OnyxThemeProps = React.PropsWithChildren & {
   theme?: "light" | "dark";
@@ -9,9 +12,9 @@ export const OnyxTheme = ({ children, ...props }: OnyxThemeProps) => {
   return (
     <div
       className={classNames(
-        props.theme === undefined && trimLines(themeLight),
-        props.theme === "light" && trimLines(themeLight),
-        props.theme === "dark" && trimLines(themeDark),
+        props.theme === undefined && "onyx-light",
+        props.theme === "light" && "onyx-light",
+        props.theme === "dark" && "onyx-dark",
         props.className
       )}
     >
@@ -19,60 +22,3 @@ export const OnyxTheme = ({ children, ...props }: OnyxThemeProps) => {
     </div>
   );
 };
-
-const trimLines = (lines: string): string => {
-  return lines
-    .split("\n")
-    .map((line) => line.trim())
-    .join(" ")
-    .trim();
-};
-
-const themeLight = `
-  bg-[#ffffff]
-  text-[#000000]
-  font-["Microsoft_Sans_Serif",sans-serif]
-  [-webkit-font-smoothing:antialiased]
-  [--dialog-border:#7b7b7b]
-  [--dialog-background:#f2f2f2]
-  [--dialog-background-header:#ffffff]
-  [--button-border:#b6b6b6]
-  [--button-background:#e5e5e5]
-  [--button-background-disabled:#d0d0d0]
-  [--button-border-focus:#1080e0]
-  [--select-border-focus:#000000]
-  [--listbox-border:#8d929b]
-  [--listbox-background-selected:#1080e0]
-  [--listbox-text-selected:#ffffff]
-  [--tabs-border:#dedede]
-  [--tabs-background:#f2f2f2]
-  [--tabs-background-hover:#ddecfa]
-  [--tabs-background-selected:#ffffff]
-  [--scrollbar-background:#f2f2f2]
-  [--scrollbar-background-thumb:#d3d3d3]
-`;
-
-const themeDark = `
-  bg-[#000000]
-  text-[#ffffff]
-  font-["Microsoft_Sans_Serif",sans-serif]
-  [-webkit-font-smoothing:antialiased]
-  [--dialog-border:#373838]
-  [--dialog-background:#212223]
-  [--dialog-background-header:#000000]
-  [--button-border:#696969]
-  [--button-background:#595959]
-  [--button-background-disabled:#2e3233]
-  [--button-border-focus:#47b5ff]
-  [--select-border-focus:#000000]
-  [--select-background:#212223]
-  [--listbox-border:#696969]
-  [--listbox-background-selected:#1080e0]
-  [--listbox-text-selected:#ffffff]
-  [--tabs-border:#3c4144]
-  [--tabs-background:#212223]
-  [--tabs-background-hover:#47b5ff]
-  [--tabs-background-selected:#373a3d]
-  [--scrollbar-background:#181818]
-  [--scrollbar-background-thumb:#575757]
-`;
